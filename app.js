b570c3bb2916dec3d9372969853d8ea9c9d616a4
Saleh-Furqan <iamsaleh.furqan@gmail.com>
2022-04-30 12:09:11 +0400

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+let currentNum = "";
+let previousNum = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector('.currentNumber')
+const previousDisplayNumber = document.querySelector('.previousNumber')
+
+const equal = document.querySelector('.equal')
+equal.addEventListener('click', calculate)
+
+const decimal = document.querySelector('.decimal')
+
+const clear = document.querySelector('.clear')
+
+
+const numberButtons = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handlenumber(e.target.textContent)
+    })
+})
+function handlenumber(number) {
+    currentNum += number; 
+    currentDisplayNumber.textContent = currentNum;
+}
+
+operators.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent)
+    })
+})
+function handleOperator(op) {
+    operator = op
+    previousNum = currentNum
+    previousDisplayNumber.textContent = previousNum + " " + operator
+    currentNum = ""
+    currentDisplayNumber.textContent = ""
+}
+
+function calculate() {
+    previousNum = Number(previousNum)
+    if (operator === "+") {
+        previousNum = previousNum + currentNum
+    }
+    previousDisplayNumber.textContent = ''
+    currentDisplayNumber.textContent = previousNum
+}
